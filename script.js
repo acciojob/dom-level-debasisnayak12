41430753a7c9a5b079bcda2b41796fa155dd2f77
Debasis Nayak <115566899+debasisnayak12@users.noreply.github.com>
2023-11-28 18:26:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let levelElement = document.getElementById("level");
+let level = 1;
+while(levelElement.parentElement){
+  level++;
+  levelElement = levelElement.parentElement;
+}
+alert(`The level of the element is:  ${level}`);
